8425f0c70b9040d74046068d6e77ceadd8de5cd4
Ivan <ivandenisovich2005@gmail.com>
2025-06-08 21:41:12 +0300

    Add basic window with fixed fps

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
 import pygame
+from checkers.constants import WIDTH, HEIGHT
+
+FPS = 60
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Checkers')
+
+
+def main():
+    run = True
+
+    # for fps
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+    pygame.quit()
+
+
+main()
