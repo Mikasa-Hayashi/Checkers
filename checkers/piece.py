0f379ac79f8d99dac212504577152b541c097634
Ivan <ivandenisovich2005@gmail.com>
2025-06-09 14:07:43 +0300

    Add assets folder and king drawing

--- checkers/piece.py
+++ checkers/piece.py
@@ -1,15 +1,15 @@
 import pygame
-from .constants import RED, WHITE, GREY, SQUARE_SIZE
+from .constants import RED, WHITE, GREY, SQUARE_SIZE, CROWN
 
 class Piece:
-    PADDING = 10
+    PADDING = 15
     OUTLINE = 2
 
     def __init__(self, row, col, color):
         self.row = row
         self.col = col
         self.color = color
-        self.king = False
+        self.king = True
 
         if self.color == RED:
             self.direction = -1
@@ -31,6 +31,9 @@ class Piece:
         radius = SQUARE_SIZE // 2 - self.PADDING
         pygame.draw.circle(win, GREY, (self.x, self.y), radius + self.OUTLINE)
         pygame.draw.circle(win, self.color, (self.x, self.y), radius)
+
+        if self.king:
+            win.blit(CROWN, (self.x - CROWN.get_width() // 2, self.y - CROWN.get_height() // 2))
 
     def __repr__(self):
         return str(self.color)
